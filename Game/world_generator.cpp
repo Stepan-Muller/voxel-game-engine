4c68339e8f64719f2d26d689f58096deec2e799a
Stepan-Muller <mullerstepan4@gmail.com>
2025-12-19 22:54:57 +0100

    lepsi generovani terenu, vyuziti vice perlinovych sumu

--- Game/world_generator.cpp
+++ Game/world_generator.cpp
@@ -4,46 +4,102 @@
 
 #include "stb_perlin.h"
 
+#define SEA_LEVEL 50
+
+#define ROUGH_PERLIN_FREQ 0.004f
+#define ROUGH_PERLIN_AMPL 40
+
+#define FINE_PERLIN_FREQ 0.01f
+#define FINE_PERLIN_AMPL 10
+
 void WorldGenerator::generateChunk(Chunk& chunk, std::pair<int, int> coord, int chunkWidth, int chunkDepth, int height)
 {
-    const float frequency = 0.004f;
-    const unsigned int maxTerrainHeight = height - 1;
-
-    for (int z = 0; z < chunkDepth; z++) {
-        for (int x = 0; x < chunkWidth; x++) {
+    for (int z = 0; z < chunkDepth; z++) 
+    {
+        for (int x = 0; x < chunkWidth; x++) 
+        {
             // perlin noise height
-            float perlin = (stb_perlin_noise3((coord.second * chunkWidth + x) * frequency, 0.0f, (coord.first * chunkDepth + z) * frequency, 0, 0, 0) + 1.0f) / 2.0f;
-            int terrainHeight = perlin * 40;
+            float roughPerlin = stb_perlin_noise3((coord.second * chunkWidth + x) * ROUGH_PERLIN_FREQ, 0.0f, (coord.first * chunkDepth + z) * ROUGH_PERLIN_FREQ, 0, 0, 0) * FINE_PERLIN_AMPL;
+            float finePerlin = stb_perlin_noise3((coord.second * chunkWidth + x) * FINE_PERLIN_FREQ, 0.0f, (coord.first * chunkDepth + z) * FINE_PERLIN_FREQ, 0, 0, 0) * FINE_PERLIN_AMPL;
+            int terrainHeight = SEA_LEVEL + roughPerlin + finePerlin + 5;
 
-            for (unsigned int y = 0; y < height; y++) {
+            for (unsigned int y = 0; y < height; y++) 
+            {
                 size_t index = x * height * chunkDepth + (height - 1 - y) * chunkWidth + z;
 
-                if (y <= terrainHeight) {
-                    // grass
-                    if (y == terrainHeight) {
-                        chunk.voxelGridColor[index * 4 + 0] = 0.2f; // R
-                        chunk.voxelGridColor[index * 4 + 1] = 0.8f; // G
-                        chunk.voxelGridColor[index * 4 + 2] = 0.2f; // B
-                        chunk.voxelGridColor[index * 4 + 3] = 1.0f; // A
+                if (y <= terrainHeight) 
+                {
+					// below terrain
+                    if (y >= terrainHeight - 4)
+					{
+						// high layers
+                        if (terrainHeight > SEA_LEVEL + 4)
+                        {
+                            // high above sea level
+                            if (y == terrainHeight)
+                            {
+                                // grass
+                                chunk.voxelGridColor[index * 4 + 0] = 0.2f; // R
+                                chunk.voxelGridColor[index * 4 + 1] = 0.8f; // G
+                                chunk.voxelGridColor[index * 4 + 2] = 0.2f; // B
+                            }
+                            else
+                            {
+                                // dirt
+                                chunk.voxelGridColor[index * 4 + 0] = 0.6f;
+                                chunk.voxelGridColor[index * 4 + 1] = 0.4f;
+                                chunk.voxelGridColor[index * 4 + 2] = 0.2f;
+                            }
+                        }
+                        else if (terrainHeight > SEA_LEVEL - 4)
+                        {
+							// beaches
+                            // sand
+							chunk.voxelGridColor[index * 4 + 0] = 0.9f;
+							chunk.voxelGridColor[index * 4 + 1] = 0.8f;
+							chunk.voxelGridColor[index * 4 + 2] = 0.5f;
+						}
+                        else
+                        {
+                            // underwater
+                            // gravel
+							chunk.voxelGridColor[index * 4 + 0] = 0.4f;
+							chunk.voxelGridColor[index * 4 + 1] = 0.4f;
+							chunk.voxelGridColor[index * 4 + 2] = 0.4f;
+                        }
+					}
+					else
+                    {
+                        // stone
+						chunk.voxelGridColor[index * 4 + 0] = 0.5f;
+						chunk.voxelGridColor[index * 4 + 1] = 0.5f;
+						chunk.voxelGridColor[index * 4 + 2] = 0.5f;
                     }
-                    else {
-                        // dirt
-                        chunk.voxelGridColor[index * 4 + 0] = 0.6f;
-                        chunk.voxelGridColor[index * 4 + 1] = 0.4f;
-                        chunk.voxelGridColor[index * 4 + 2] = 0.2f;
-                        chunk.voxelGridColor[index * 4 + 3] = 1.0f;
-                    }
-
+                    chunk.voxelGridColor[index * 4 + 3] = 1.0f;
                     chunk.voxelGridProperties[index] = 0.0f;
                     chunk.voxelGridCollision[index] = true;
                 }
-                else {
-                    // air
-                    chunk.voxelGridColor[index * 4 + 0] = 0.0f;
-                    chunk.voxelGridColor[index * 4 + 1] = 0.0f;
-                    chunk.voxelGridColor[index * 4 + 2] = 0.0f;
-                    chunk.voxelGridColor[index * 4 + 3] = 0.0f;
-                    chunk.voxelGridProperties[index] = 0.0f;
+                else 
+                {
+					// above terrain
+                    if (y <= SEA_LEVEL) 
+                    {
+						// water
+						chunk.voxelGridColor[index * 4 + 0] = 0.2f;
+						chunk.voxelGridColor[index * 4 + 1] = 0.5f;
+						chunk.voxelGridColor[index * 4 + 2] = 1.0f;
+						chunk.voxelGridColor[index * 4 + 3] = 0.8f;
+						chunk.voxelGridProperties[index] = 0.2f;
+					}
+                    else
+                    {
+                        // air
+                        chunk.voxelGridColor[index * 4 + 0] = 0.0f;
+                        chunk.voxelGridColor[index * 4 + 1] = 0.0f;
+                        chunk.voxelGridColor[index * 4 + 2] = 0.0f;
+                        chunk.voxelGridColor[index * 4 + 3] = 0.0f;
+                        chunk.voxelGridProperties[index] = 0.0f;
+                    }
                     chunk.voxelGridCollision[index] = false;
                 }
             }
--- voxel-game-engine/player.h
+++ voxel-game-engine/player.h
@@ -34,97 +34,97 @@ public:
     Player(Map* _map);
 private:
     /**
-	 * @brief Pointer to the map object in which the player is located.
+     * @brief Pointer to the map object in which the player is located.
      */
     Map map;
-    
+
     /**
-	 * @brief Position of the player in the map.
+     * @brief Position of the player in the map.
      */
     float pos[3];
 
-	int lastChunkPos[2];
-    
+    int lastChunkPos[2];
+
     /**
-	 * @brief Angle of the player in the map.
+     * @brief Angle of the player in the map.
      */
     float angle[2];
-    
+
     /**
      * @brief Last cursor position.
      */
     float lastMouse[2];
-    
+
     /**
      * @brief Sspeed at which the player is currently falling (<0 => the player is rising up).
      */
     float fallSpeed = 0;
-    
+
     /**
      * @brief Delta time.
      */
     float deltaTime;
-    
+
     /**
      * @brief Speed at which the player turns (mouse sensitivity).
      */
     float turnSpeed = 0.002f;
-    
+
     /**
      * @brief Field of view of the camera in radians.
      */
     float fov = 60 * PI / 180.0f;
-    
+
     /**
      * @brief timer for footstep sounds.
      */
     float stepTimer = 0.0f;
-    
+
     /**
      * @brief Width of the screen in pixels.
      */
     int screenWidth = 1280;
-    
+
     /**
      * @brief Height of the screen in pixels.
      */
     int screenHeight = 720;
-    
+
     /**
      * @brief Render distance of the camera in voxels.
      */
     int renderDistance = 5;
-    
+
     /**
      * @brief Whether the player is currently in the menu.
      */
     bool menu = false;
-    
+
     /**
      * @brief Flag for reseting the mouse position when exiting out of the menu.
      */
     bool resetMouse = true;
-    
+
     /**
      * @brief Whether the window should use v-sync.
      */
     bool vSync = true;
-    
+
     /**
      * @brief Whether the player is currently standing on the ground.
      */
     bool grounded = false;
-    
+
     /**
      * @brief Buffer for the voxel in the middle of the screen, used fo world interaction.
      */
     GLuint hitBuffer;
-    
+
     /**
      * @brief Sound system.
      */
     Sound sound;
-    
+
     /**
      * @brief Pointer to the gui class.
      */
@@ -137,32 +137,32 @@ private:
      * @return The shader source code as a string.
      */
     std::string loadShaderSource(const std::string& filePath);
-    
+
     /**
      * @brief Respawn the player at the maps spawn position and angle.
      */
     void respawn();
-    
+
     /**
      * @brief GLFW key callback.
      */
     void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
-    
+
     /**
      * @brief GLFW cursor position callback.
      */
     void mouseCallback(GLFWwindow* window, double xpos, double ypos);
-    
+
     /**
      * @brief GLFW mouse button callback.
      */
     void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
-    
+
     /**
      * @brief GLFW window size callback.
      */
     void windowSizeCallback(GLFWwindow* window, int width, int height);
-    
+
     /**
      * @brief Check whether the player is colliding with the map at the specified location.
      *
@@ -170,29 +170,29 @@ private:
      * @return True if there is a collision, false otherwise.
      */
     bool checkPlayerCollision(float pos[3]);
-    
+
     /**
      * @brief Move the player based on input and check for collisions.
      *
      * @param window The GLFW window.
      */
     void movePlayer(GLFWwindow* window);
-    
+
     /**
      * @brief GLFW key callback, made static.
      */
     static void staticKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
-    
+
     /**
      * @brief GLFW cursor position callback, made static.
      */
     static void staticMouseCallback(GLFWwindow* window, double xpos, double ypos);
-    
+
     /**
      * @brief GLFW mouse button callback, made static.
      */
     static void staticMouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
-    
+
     /**
      * @brief GLFW window size callback, made static.
      */
